bcdecfa60d05d7be775c9e76e222bb29bba45641
ChrisSR0 <Chriissr777@gmail.com>
2026-02-07 20:23:38 -0500

    feat: middleware 404 para rutas no encontradas

--- Backend/index.js
+++ Backend/index.js
@@ -7,6 +7,8 @@ import healthRoute from './src/routes/health.routes.js';
 
 //  config de cors(lueugo la hago)
 
+// middleware para la rutas no encontradas
+import {notFoundHandler} from './src/middlewares/notFound.js'
 
 
 const app = express() // se crea la app(instacia de express)
@@ -21,7 +23,8 @@ app.use('/health', healthRoute);
 
 
 // manejo de errores (luego)
+// si llego aqui => ninguana ruta coincidio = 404
+app.use(notFoundHandler);
 
 
-
-export default  app;
+export default app;
